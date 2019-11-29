d796a275721cf80dd81e77fb96a6ce450b5dc584
hello413 <2827717859@qq.com>
2019-11-30 01:37:43 +0800

    practical

--- C-code/100道经典例题/58-63.c
+++ C-code/100道经典例题/58-63.c
@@ -7,6 +7,97 @@
   实现strchr()
   实现strstr()*/
 #pragma warning(disable:4996)
+
+
+int strlen(char* str1)
+{
+	if (*str1 == NULL)
+	{
+		printf("输入字符串有误\n");
+		return NULL;
+	}
+	int i = 0;
+	while (str1[i++] != '\0')
+	{
+		;
+	}
+	return --i;
+}
+
+int strcmp(char* str1, char* str2)
+{
+	while (1)
+	{
+		if (*str1 == '\0' || *str2 == '\0')
+			return 0;
+		int i = 0;
+		if (str1 != str2)
+		{
+			return str1[i] - str2[i];
+		}
+		i++;
+	}
+
+}
+
+char* strchr(char *str1, char ch)
+{
+	while (*str1 != '\0' && *str1 != ch)
+	{
+		++str1;
+	}
+	return *str1 == ch ? str1 : NULL;
+}
+
+char* strstr(char *str1, char *str2)
+{
+	int n = strlen(str2);
+	for (; *str1 != 0; ++str1) {
+		if (strncmp(str1, str2, n) == 0) {
+			return (char*)str1;
+		}
+	}
+	return NULL;
+}
+
+char *strcpy(char* str1, const char* str2)
+{
+	if (*str1 == NULL || *str2 == NULL)
+	{
+		printf("输入字符串有误\n");
+		return NULL;
+	}
+	int i = 0;
+	while (1)
+	{
+		str1[i] = str2[i];
+		if (str1[i++] == '\0')
+			break;
+	}
+	return str1;
+}
+
+char *strcat(char* str1, const char* str2)
+{
+	if (*str1 == NULL || *str2 == NULL)
+	{
+		printf("输入字符串有误\n");
+		return NULL;
+	}
+	int i = 0, j = 0;
+	while (str1[i++] != '\0')
+	{
+		;
+	}
+	i--;
+	while (str1[j] != '\0')
+	{
+		str1[i + j] = str2[j];
+		j++;
+	}
+	return str1;
+}
+
 int main()
 {
 	char arr1[20] = { "hello world" };
